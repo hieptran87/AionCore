60787af63d007236c1561e10b064f5ecd87cfbf2
Hiep Tran <hiep.tran@digitas.com>
2026-08-17 15:27:59 +0700

    test(channel): add Zalo integration test suite

---
+++ crates/aionui-channel/tests/zalo_integration.rs
@@ -0,0 +1,21 @@
+use aionui_channel::plugin::ChannelPlugin;
+use aionui_channel::plugins::create_plugin;
+use aionui_channel::types::{
+    PluginConfig, PluginCredentials, PluginStatus, PluginType,
+};
+
+#[test]
+fn test_create_zalo_plugin() {
+    let plugin_opt = create_plugin(PluginType::Zalo);
+    #[cfg(feature = "zalo")]
+    {
+        assert!(plugin_opt.is_some());
+        let plugin = plugin_opt.unwrap();
+        assert_eq!(plugin.plugin_type(), PluginType::Zalo);
+        assert_eq!(plugin.status(), PluginStatus::Created);
+    }
+    #[cfg(not(feature = "zalo"))]
+    {
+        assert!(plugin_opt.is_none());
+    }
+}
